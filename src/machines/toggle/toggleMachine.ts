6fbb134fad89944d7d27e6f4eedfb4891643c17e
Peter Jaber <peter@cloudanalyser.com>
2024-01-12 10:49:53 +1100

    initial

---
+++ src/machines/toggle/toggleMachine.ts
@@ -0,0 +1,20 @@
+import { createMachine, createActor } from 'xstate';
+
+export const toggleMachine = createMachine({
+    id: 'toggle',
+    initial: 'inactive',
+    states: {
+        inactive: {
+            on: {
+                toggle: 'active'
+            }
+        },
+        active: {
+            on: {
+                toggle: 'inactive'
+            }
+        }
+    }
+});
+
+
